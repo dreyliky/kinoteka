09d2f38fc080b3ffbc1397f60bd527ed7e90b2d1
DreyLiky <dreyliky@gmail.com>
2022-06-02 20:20:59 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -19,6 +20,12 @@ import { AppRoutingModule } from './app.routing';
         HttpClientModule,
         SharedModule,
         HeaderModule
+    ],
+    providers: [
+        {
+            provide: LocationStrategy,
+            useClass: HashLocationStrategy
+        }
     ],
     bootstrap: [
         AppComponent
