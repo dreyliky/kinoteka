6c7266376bdc4012995ec4cdcaae916b8b163361
DreyLiky <dreyliky@gmail.com>
2022-06-06 14:58:51 +0300

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: true,
-  backendWsHost: 'ws://192.168.1.23:3001'
+    production: true,
+    backendHost: 'http://192.168.1.23:3000',
+    backendWsHost: 'ws://192.168.1.23:3001'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  backendWsHost: 'ws://localhost:3001'
+    production: false,
+    backendHost: 'http://localhost:3000',
+    backendWsHost: 'ws://localhost:3001'
 };
 
 /*
